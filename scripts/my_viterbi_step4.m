bab355143be1014396f871e5f2a4b3a1acc11a4b
SaiKrishna <saikrishna.r@research.iiit.ac.in>
2015-04-12 09:25:34 +0530

    Added the OOPised code_v1

---
+++ scripts/my_viterbi_step4.m
@@ -0,0 +1,18 @@
+function exemplar_array = my_viterbi_step4(test_phones, phones_dump, k_i)
+% Find the units
+exemplar_array = [];
+for i = 1:length(test_phones)
+       
+    unit = test_phones(i);
+    if strmatch(unit, 'SIL')
+        p = p + 1;
+    else
+        unit
+        unit_examples = find(strcmp(test_phones(i),temp)>0);
+        best_cost = k_i(i);
+        exemplar = unit_examples(best_cost);
+        exemplar_array = [ exemplar_array exemplar];
+    end
+    
+end
+end
